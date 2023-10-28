78c0d0f1a3d3d4a0e49132d06ceb8583e254eabe
Leon <92170476+L50N@users.noreply.github.com>
2023-10-28 21:04:09 +0200

    + login component

--- firewall-dashboard/pages/index.vue
+++ firewall-dashboard/pages/index.vue
@@ -10,7 +10,10 @@ const route = useRoute()
 </template>
 
 <style>
-body {
+  @import url('https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,200;9..40,300;9..40,400;9..40,500;9..40,600;9..40,700;9..40,800;9..40,900;9..40,1000&family=Inter:wght@300;400;500;600;700;800&display=swap');
+
+  body {
   background-color: #F6F6F6;
+  font-family: 'DM Sans', sans-serif;
 }
 </style>
