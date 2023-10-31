ce880ce059471e22cd6ed46e1f03aa0d5974e48e
Leon <bartnikleon@proton.me>
2023-10-31 12:53:18 +0100

    + font import site loading error fixed (hotfix)

--- firewall-dashboard/pages/groups.vue
+++ firewall-dashboard/pages/groups.vue
@@ -7,6 +7,6 @@
 </template>
 
 <style scoped>
-// FONT IMPORT ON SITE LOAD
+/* FONT IMPORT ON SITE LOAD */
 @import url('https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,200;9..40,300;9..40,400;9..40,500;9..40,600;9..40,700;9..40,800;9..40,900;9..40,1000&family=Inter:wght@300;400;500;600;700;800&display=swap');
 </style>
--- firewall-dashboard/pages/index.vue
+++ firewall-dashboard/pages/index.vue
@@ -10,7 +10,7 @@ const route = useRoute()
 </template>
 
 <style>
-  // FONT IMPORT ON SITE LOAD
+/* FONT IMPORT ON SITE LOAD */
   @import url('https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,200;9..40,300;9..40,400;9..40,500;9..40,600;9..40,700;9..40,800;9..40,900;9..40,1000&family=Inter:wght@300;400;500;600;700;800&display=swap');
 
   body {
