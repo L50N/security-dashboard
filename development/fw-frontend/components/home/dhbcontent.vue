69d5258d905f4e482777ef28ee0ed9330a68fde6
Leon <bartnikleon@proton.me>
2023-11-04 00:11:10 +0100

    + changed grid in home/dhbcontent

--- development/fw-frontend/components/home/dhbcontent.vue
+++ development/fw-frontend/components/home/dhbcontent.vue
@@ -3,11 +3,11 @@
 </script>
 
 <template>
-  <div class="grid grid-cols-[5fr,3fr] 2xl:grid-cols-[1fr,450px]">
+  <div class="grid grid-cols-[5fr,3fr] 2xl:grid-cols-[1fr,465px]">
     <homeContentHello />
     <homeContentPercentage />
   </div>
-  <div class="grid grid-cols-[1fr,1fr,475px] -mt-[12.5rem]">
+  <div class="grid grid-cols-[1fr,1fr,485px] -mt-[12.5rem]">
     <homeContentGroups />
     <homeContentUpdate />
   </div>
