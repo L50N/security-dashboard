74ffcf3a95792c0810e48fc0102ccacc15792849
Leon <92170476+L50N@users.noreply.github.com>
2024-01-23 22:15:41 +0100

    + (edited) error page - redirect to "/"

--- frontend/error.vue
+++ frontend/error.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8 items-center h-screen">
-    <div class="text-center">
-      <p class="text-base font-semibold text-primary">404</p>
-      <p class="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Page not found</p>
-      <p class="mt-3 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
-      <div class="mt-6 flex items-center justify-center gap-x-6">
-        <a href="/"
-          class="rounded-md bg-primary px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:opacity-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go
-          back home</a>
-        <a href="https://github.com/L50N" target="_blank" class="text-sm font-semibold text-gray-900">Get help <span
-            aria-hidden="true">&rarr;</span></a>
-      </div>
-    </div>
-  </div></template>
+  <div>
+  </div>
+  </template>
+  
+  <script>
+function redirect(){
+    window.location.href = '/';
+}
+
+window.onload = function(){
+    redirect();
+}
+
+  </script>
